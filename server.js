342528e0ec54f5988d3739e8c66272a8d3f04f70
baumertjonathan <bau14003@byui.edu>
2018-11-27 18:41:05 -0700

    test

--- server.js
+++ server.js
@@ -1,54 +1,6 @@
-var express = require("express");
-var app = express();
-var port = 3000;
-//connecting to database
+const express = require('express');
+const app = express();
 
-const { Client } = require('pg');
+app.use(express.static('public'));
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
-//database connected
-
-app.use(express.static("public"));
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-
-app.get("/", function(req, res) {
-	console.log("Received a request for /");
-
-	res.write("This is the root.");
-	res.end();
-});
-
-app.get("/display", function(req, res){
-	console.log("display called");
-
-	var cost = 5;
-	var weight = 9;
-
-	var params = {cost: cost, weight: weight}
-	res.render("display", params);
-		res.end();
-});
-
-app.get("/aTest", function(req, res) {
-	res.write("this is a test");
-	console.log("a test has been done");
-	res.end();
-});
-
-
-app.listen(port, () => console.log("example app listening on port 3000!"))
+app.listen(process.env.PORT || 3000, ()=>console.log("request made"));
