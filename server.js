39840c2887adc2d6abd02d6b68dcc98fc0df24ec
baumertjonathan <bau14003@byui.edu>
2018-11-27 18:45:23 -0700

    servertest

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
+app.set("views", "views");
+app.set("view engine", "ejs");
 
 //database connection start
 const { Client } = require('pg');
